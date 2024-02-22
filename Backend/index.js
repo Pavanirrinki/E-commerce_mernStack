34e3ca3be61cf547b47757d03e81b941ef32f812
unknown <pavankumarirrinki0123@gmail.com>
2024-02-23 00:44:24 +0530

    vercel api change.

--- Backend/index.js
+++ Backend/index.js
@@ -13,11 +13,8 @@ const port = process.env.PORT || 8000
 
 
 
-
+app.use(cors());
 app.use(express.json());
-app.use(cors({
-  origin:"*"
-}))
 
 app.use(express.urlencoded({extended:true}))
 mongoose.connect(process.env.MONGO_URI).then(() => {
