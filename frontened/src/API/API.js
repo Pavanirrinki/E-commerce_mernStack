bd8050ceb103b74f020ae0777191cf5be62fe0b5
unknown <pavankumarirrinki0123@gmail.com>
2024-02-23 00:01:10 +0530

    vercel api change.

--- frontened/src/API/API.js
+++ frontened/src/API/API.js
@@ -1,3 +1,3 @@
 // export  const API = "http://localhost:3070/";
 // export const API = "https://09e0-45-112-185-12.ngrok-free.app/";
-export  const API = "https://e-commerce-mern-stack-dnpwm2601-pavanirrinki.vercel.app/"
+export  const API = "https://e-commerce-mern-stack-oxgdq0zjg-pavanirrinki.vercel.app/"
