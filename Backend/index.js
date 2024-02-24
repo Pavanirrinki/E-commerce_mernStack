88d5eda50a40dee4322c4b77ad21f0d6e205df2b
unknown <pavankumarirrinki0123@gmail.com>
2024-02-24 17:46:19 +0530

    payment route urls change

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 8000
 
 app.use(express.json());
 app.use(cors({
-  origin: 'e-commerce-mern-stack-zt62-git-main-pavanirrinki.vercel.app' // Replace 'http://example.com' with your desired URL
+  origin: 'https://e-commerce-mern-stack-ke5u.vercel.app/' // Replace 'http://example.com' with your desired URL
 }))
 app.use(express.urlencoded({extended:true}))
 mongoose.connect(process.env.MONGO_URI).then(() => {
