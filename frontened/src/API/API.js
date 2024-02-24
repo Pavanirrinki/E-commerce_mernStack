501542db51728a03364cbdcb8ea0b1b570155679
unknown <pavankumarirrinki0123@gmail.com>
2024-02-24 18:04:52 +0530

    payment route urls change

--- frontened/src/API/API.js
+++ frontened/src/API/API.js
@@ -1,3 +1,3 @@
 // export  const API = "http://localhost:3070/";
 // export const API = "https://09e0-45-112-185-12.ngrok-free.app/";
-export  const API = "https://e-commerce-mern-stack-f4cbctoqn-pavanirrinki.vercel.app/"
+export  const API = "https://e-commerce-mern-stack-cjxrfkmxt-pavanirrinki.vercel.app/"
